343f4378a4f11973c2a0af6429e1e009183c714d
guillem <guillem.vtorrent@gmail.com>
2022-04-25 00:35:38 +0200

    auth done

--- routes/web.php
+++ routes/web.php
@@ -12,28 +12,33 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Models\Image; 
+// use App\Models\Image; 
 
 Route::get('/', function () {
 
-    $images = Image::all(); 
-    foreach($images as $image) {
-        echo $image->image_path."<br/>"; 
-        echo $image->description."<br/>"; 
-        echo $image->user->name.' '.$image->user->surname."<br/>"; // No need for inner joins
+    // $images = Image::all(); 
+    // foreach($images as $image) {
+    //     echo $image->image_path."<br/>"; 
+    //     echo $image->description."<br/>"; 
+    //     echo $image->user->name.' '.$image->user->surname."<br/>"; // No need for inner joins
 
 
-        if($image->comments) {
-            echo '<strong>Comments: </strong><br/>'; 
-            foreach($image->comments as $comment) {
-                echo $comment->user->name.' '.$comment->user->surname.': '; 
-                echo $comment->content."<br/>"; 
-            }
-        }
-        echo 'LIKES: '.count($image->likes); 
-        echo "<hr/>"; 
-    }
+    //     if($image->comments) {
+    //         echo '<h4>Comments: </h4><br/>'; 
+    //         foreach($image->comments as $comment) {
+    //             echo $comment->user->name.' '.$comment->user->surname.': '; 
+    //             echo $comment->content."<br/>"; 
+    //         }
+    //     }
+    //     echo 'LIKES: '.count($image->likes); 
+    //     echo "<hr/>"; 
+    // }
 
-    die(); 
+    // die(); 
+
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
